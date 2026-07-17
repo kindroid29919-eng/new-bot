009e1085e1fcc3d9411aa1b53c5289b0ac1fc872
kindroid29919-eng <kindroid29919@gmail.com>
2026-07-17 22:57:47 +0500

    Add files via upload

---
+++ discord-bot/data/kisses.js
@@ -0,0 +1,16 @@
+module.exports = {
+  styles: [
+    'a sweet peck on the cheek', 'a dramatic movie-style kiss', 'a shy forehead kiss',
+    'a sneaky surprise kiss', 'a gentle hand kiss', 'a classic kiss under the stars',
+    'a big cartoon smooch', 'a tender goodnight kiss', 'a playful blown kiss',
+    'a wholesome nose boop kiss', 'a slow-motion anime kiss', 'a quick hello kiss'
+  ],
+  messages: [
+    'Aww, sparks are flying!', 'Their face turned bright red!', 'Butterflies everywhere!',
+    'A moment straight out of a romance anime.', 'They melted on the spot.',
+    'The room went silent in awe.', 'Cue the sparkly background effects.',
+    'They\'ll be smiling about that all day.', 'Relationship status: complicated but happy.',
+    'That definitely counts as a plot twist.'
+  ],
+  emojis: ['😘', '💋', '💕', '🥰', '✨', '💗', '🌹']
+};
